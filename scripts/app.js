2f6f931d1a174e3014d45c4794f321aeb48f8276
Victoria Goltseva <katnisseverdeen191@mail.ru>
2023-10-08 23:18:31 +0300

    plus loader

--- scripts/app.js
+++ scripts/app.js
@@ -1,48 +1,66 @@
-const intro = document.querySelector('.intro');
-const video = intro.querySelector('video');
-const text = intro.querySelector('h1');
+let loaderWrapper = document.getElementById('loader-wrapper');
+loaderWrapper.style.display = 'flex';
+
+let body = document.querySelector('body');
+
+console.log(3);
+
+
+window.onload = function () {
+    console.log(2);
+
+    const intro = document.querySelector('.intro');
+    const video = intro.querySelector('video');
+    const text = intro.querySelector('h1');
 //END SECTION
-const section = document.querySelector('section');
-const end = section.querySelector('h2');
+    const section = document.querySelector('section');
+    const end = section.querySelector('h2');
 
 //SCROLLMAGIC
-const controller = new ScrollMagic.Controller();
+    const controller = new ScrollMagic.Controller();
 
 //Scenes
-let scene = new ScrollMagic.Scene({
-    duration: 9000,
-    triggerElement: intro,
-    triggerHook: 0
-})
-    .setPin(intro)
-    .addTo(controller);
+    let scene = new ScrollMagic.Scene({
+        duration: 9000,
+        triggerElement: intro,
+        triggerHook: 0
+    })
+        .setPin(intro)
+        .addTo(controller);
 
 
 //Text Animation
-const textAnim = TweenMax.fromTo(text, 3, {opacity: 1}, {opacity: 0});
+    const textAnim = TweenMax.fromTo(text, 3, {opacity: 1}, {opacity: 0});
 
-let scene2 = new ScrollMagic.Scene({
-    duration: 1000,
-    triggerElement: intro,
-    triggerHook: 0
-})
-    .setTween(textAnim)
-    .addTo(controller);
+    let scene2 = new ScrollMagic.Scene({
+        duration: 1000,
+        triggerElement: intro,
+        triggerHook: 0
+    })
+        .setTween(textAnim)
+        .addTo(controller);
 
 //Video Animation
-let accelamount = 0.1;
-let scrollpos = 0;
-let delay = 0;
+    let accelamount = 0.1;
+    let scrollpos = 0;
+    let delay = 0;
 
-scene.on("update", e => {
-    scrollpos = e.scrollPos / 1000;
-});
+    scene.on("update", e => {
+        scrollpos = e.scrollPos / 1000;
+    });
 
-setInterval(() => {
-    delay += (scrollpos - delay) * accelamount;
+    setInterval(() => {
+        delay += (scrollpos - delay) * accelamount;
 
-    video.currentTime = delay;
-}, 33.3);
+        video.currentTime = delay;
+    }, 33.3);
 
+
+
+
+    body.style.overflowY = 'scroll';
+    loaderWrapper.style.display = 'none';
+
+}
 
 
